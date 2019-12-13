1400cd3880e78ef3c91db9623135ea24e57d936c
Kistanov <rizzydizzy@mail.ru>
2019-12-13 18:34:33 +0300

    #002 Done, without edits

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,6 +4,8 @@ module.exports = function() {
             .pipe($.gp.pug({
                 locals : {
                     nav: JSON.parse($.fs.readFileSync('./data/navigation.json', 'utf8')),
+                    press: JSON.parse($.fs.readFileSync('./data/pages/press.json', 'utf8')),
+                    contact: JSON.parse($.fs.readFileSync('./data/pages/contact.json', 'utf8')),
                     global: JSON.parse($.fs.readFileSync('./data/global.json', 'utf8')),
                     index: JSON.parse($.fs.readFileSync('./data/pages/index.json', 'utf8')),
                     smpo: JSON.parse($.fs.readFileSync('./data/pages/smpo.json', 'utf8')),
